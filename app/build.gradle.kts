4df86fd289dad169ae6035dfcf343ad9b75bd052
bonfacemasira <bonfacemasira@gmail.com>
2025-01-16 02:29:05 +0300

    calculate tip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.tipcalcuator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tipcalcuator"
